4f9f2e6674cdcdff1ae7fa0f2bb729237e543cbe
eniolajinadu <eniolajinadu792@gmail.com>
2024-08-13 15:55:12 +0100

    updated files

---
+++ js.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+var Name = 'namesss';
+console.log(Name)
+
+let toda = 23
+console.log(toda)
+console.log(typeof(toda))
+
+prompt("Do you know what you're doing is wrong?")
